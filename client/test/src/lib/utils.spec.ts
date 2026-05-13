dd354f5632b8f012a24d0687ccd88e4545ba957d
arthurfaby <arthurfaby1@gmail.com>
2026-05-13 21:55:34 +0200

    feat: enhance utility tests and fix coverage pattern in Vitest configuration

--- client/test/src/lib/utils.spec.ts
+++ client/test/src/lib/utils.spec.ts
@@ -5,6 +5,7 @@ describe("utils.ts", () => {
   describe("cn", () => {
     it("should merge tailwind classes", () => {
       expect(cn(["bg-red-400", "text-sm"])).toEqual("bg-red-400 text-sm");
+      expect(cn("bg-red-400", "text-sm")).toEqual("bg-red-400 text-sm");
     });
   });
 });
