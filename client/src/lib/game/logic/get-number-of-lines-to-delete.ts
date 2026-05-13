a74559eff5fe46e12849630e7b8ddf2fd38576a9
arthurfaby <arthurfaby1@gmail.com>
2026-05-13 21:40:36 +0200

    feat: simplify line deletion calculation by using filter method in getNumberOfLinesToDelete

--- client/src/lib/game/logic/get-number-of-lines-to-delete.ts
+++ client/src/lib/game/logic/get-number-of-lines-to-delete.ts
@@ -1,14 +1,8 @@
 import { Tetromino, type TetrominoType } from "@red-tetris/shared";
 
 export function getNumberOfLinesToDelete(board: TetrominoType[][]): number {
-  let linesToDelete = 0;
-  for (let i = board.length - 1; i >= 0; i--) {
-    const row = board[i];
-    const isLineFull = row.every((cell) => cell !== Tetromino.NONE);
-    if (isLineFull) {
-      linesToDelete += 1;
-    }
-  }
-
-  return linesToDelete;
+  const clearedBoard = board.filter(
+    (row) => !row.every((cell) => cell !== Tetromino.NONE),
+  );
+  return board.length - clearedBoard.length;
 }
