249ba7f899d176053182d82f549465913dfcb2c7
pirabaud <rabaudp@gmail.com>
2026-05-20 16:57:09 +0200

    feat: Synchronized the 7-bag system across all players to ensure identical piece progression.
    # Conflicts:
    #	client/src/lib/stores/use-tetris-store.ts
    #	server/src/types.ts

--- server/src/socket/index.ts
+++ server/src/socket/index.ts
@@ -1,9 +1,9 @@
 import { FastifyInstance } from 'fastify'
-import { handlerSocketConnection } from "./handlers/room";
-import {ClientToServerEvents} from "../types";
-import {Socket} from "socket.io";
-import {RoomsManager} from "./managers/RoomsManager";
-import {handlerGame} from "./handlers/game";
+import { handlerSocketConnection } from './handlers/room'
+import { ClientToServerEvents } from '../types'
+import { Socket } from 'socket.io'
+import { RoomsManager } from './managers/RoomsManager'
+import { handlerGame } from './handlers/game'
 
 export const registerSocketHandlers = (app: FastifyInstance) => {
     const roomsManager = new RoomsManager()
--- server/src/socket/managers/RoomsManager.ts
+++ server/src/socket/managers/RoomsManager.ts
@@ -1,18 +1,17 @@
-import {Piece} from "../../domain/Piece";
+import { Piece } from '../../domain/Piece'
 
 export class RoomsManager {
     private rooms: Map<string, Piece> = new Map()
 
     public newRoom(room: string, listPlayer: string[]) {
         if (this.rooms.has(room)) {
-            return;
-        }
-        else {
-            this.rooms.set(room, new Piece(listPlayer));
+            return
+        } else {
+            this.rooms.set(room, new Piece(listPlayer))
         }
     }
 
     public getPiece(room: string) {
-        return this.rooms.get(room);
+        return this.rooms.get(room)
     }
 }
