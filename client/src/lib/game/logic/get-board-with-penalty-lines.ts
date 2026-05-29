0a9d553cbfb874de8499c85669e3b364263e3cc9
arthurfaby <arthurfaby1@gmail.com>
2026-05-29 14:53:00 +0200

    feat: implement penalty lines feature and update game logic for handling penalties

--- client/src/lib/game/logic/get-board-with-penalty-lines.ts
+++ client/src/lib/game/logic/get-board-with-penalty-lines.ts
@@ -4,6 +4,7 @@ export function getBoardWithPenaltyLines(
   board: TetrominoType[][],
   numberOfPenaltyLines: number,
 ) {
+  // Add penalty lines at bottom of board
   const penaltyLines = Array.from({ length: numberOfPenaltyLines }, () =>
     new Array<TetrominoType>(GRID_WIDTH).fill(Tetromino.PENALTY),
   );
