34acdea0b9aeb6222686df8238e93bdd1d5b335c
pirabaud <rabaudp@gmail.com>
2026-06-05 11:03:11 +0200

    feat: add bundle

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -13,4 +13,23 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (
+            id.includes("node_modules/react/") ||
+            id.includes("node_modules/react-dom/")
+          ) {
+            return "vendor";
+          }
+          if (id.includes("node_modules/react-router-dom/")) {
+            return "router";
+          }
+        },
+      },
+    },
+  },
 });
