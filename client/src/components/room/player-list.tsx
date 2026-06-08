e683a25ec927b9d4f198713caccf2e680bacccc4
arthurfaby <arthurfaby1@gmail.com>
2026-06-08 17:29:16 +0200

    feat: simplify JSX structure in player list component for improved readability

--- client/src/components/room/player-list.tsx
+++ client/src/components/room/player-list.tsx
@@ -17,21 +17,19 @@ export function PlayerList({ players, leaderId }: PlayerListProps) {
     const userFallback =
       (userSplit[0].at(0) ?? "") + (userSplit[1]?.at(0) ?? "");
     return (
-      <>
-        <div key={user.id} className="flex items-center ">
-          <div className="flex items-center gap-2">
-            <Avatar>
-              {leaderId === user.id && (
-                <AvatarBadge className=" ring-1 bg-secondary">
-                  <Crown className="size-24 text-amber-400" />
-                </AvatarBadge>
-              )}
-              <AvatarFallback>{userFallback}</AvatarFallback>
-            </Avatar>
-            <p>{user.username}</p>
-          </div>
+      <div key={user.id} className="flex items-center ">
+        <div className="flex items-center gap-2">
+          <Avatar>
+            {leaderId === user.id && (
+              <AvatarBadge className=" ring-1 bg-secondary">
+                <Crown className="size-24 text-amber-400" />
+              </AvatarBadge>
+            )}
+            <AvatarFallback>{userFallback}</AvatarFallback>
+          </Avatar>
+          <p>{user.username}</p>
         </div>
-      </>
+      </div>
     );
   });
 }
