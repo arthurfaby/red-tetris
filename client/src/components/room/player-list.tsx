a5379e543e42f3ed72db433ae735b8c96ca6cce3
arthurfaby <arthurfaby1@gmail.com>
2026-06-08 17:21:44 +0200

    feat: enhance game logic for solo mode and improve player knockout handling

--- client/src/components/room/player-list.tsx
+++ client/src/components/room/player-list.tsx
@@ -17,19 +17,21 @@ export function PlayerList({ players, leaderId }: PlayerListProps) {
     const userFallback =
       (userSplit[0].at(0) ?? "") + (userSplit[1]?.at(0) ?? "");
     return (
-      <div key={user.id} className="flex items-center ">
-        <div className="flex items-center gap-2">
-          <Avatar>
-            {leaderId === user.id && (
-              <AvatarBadge className=" ring-1 bg-secondary">
-                <Crown className="size-24 text-amber-400" />
-              </AvatarBadge>
-            )}
-            <AvatarFallback>{userFallback}</AvatarFallback>
-          </Avatar>
-          <p>{user.username}</p>
+      <>
+        <div key={user.id} className="flex items-center ">
+          <div className="flex items-center gap-2">
+            <Avatar>
+              {leaderId === user.id && (
+                <AvatarBadge className=" ring-1 bg-secondary">
+                  <Crown className="size-24 text-amber-400" />
+                </AvatarBadge>
+              )}
+              <AvatarFallback>{userFallback}</AvatarFallback>
+            </Avatar>
+            <p>{user.username}</p>
+          </div>
         </div>
-      </div>
+      </>
     );
   });
 }
--- server/src/domain/Player.ts
+++ server/src/domain/Player.ts
@@ -11,7 +11,7 @@ export class Player {
         username: string
     ) {
         this.#username = username
-        this.#isDead = false
+        this.#isDead = true
     }
 
     get isDead(): boolean {
@@ -23,7 +23,7 @@ export class Player {
     }
 
     resetState() {
-        this.#isDead = false
+        this.#isDead = true
     }
 
     get username() {
