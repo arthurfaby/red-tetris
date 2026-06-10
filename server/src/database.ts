0520e9fbc5372f04dc6e24144b05328519eda9a1
pirabaud <rabaudp@gmail.com>
2026-06-10 19:51:38 +0200

    feat: add and config db sqlite
    # Conflicts:
    #	package.json
    #	pnpm-lock.yaml

---
+++ server/src/database.ts
@@ -0,0 +1,20 @@
+import {FastifyPluginAsync} from "fastify";
+import Database from "better-sqlite3";
+import fp from 'fastify-plugin'
+
+const sqlitePlugin: FastifyPluginAsync = async (fastify) => {
+    const db = new Database('./app.db')
+
+    db.exec(`
+    CREATE TABLE IF NOT EXISTS leaderboard (
+      id    INTEGER PRIMARY KEY AUTOINCREMENT,
+      name  TEXT NOT NULL,
+      score INTEGER UNIQUE NOT NULL
+    )
+  `)
+
+    fastify.decorate('db', db)
+    fastify.addHook('onClose', () => db.close())
+}
+
+export default fp(sqlitePlugin, { name: 'sqlite' })
--- server/src/types.ts
+++ server/src/types.ts
@@ -1,7 +1,9 @@
 import { SocketServer } from '@red-tetris/shared'
+import Database from "better-sqlite3";
 
 declare module 'fastify' {
     interface FastifyInstance {
         io: SocketServer
+        db: Database.Database
     }
 }
